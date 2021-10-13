6c41170bbbacdde9bbcf17c1c9e868e08fe8d7a8
Krysl <krysl@qq.com>
2021-10-13 22:00:17 +0800

    Simply render the image by D3.js
    Signed-off-by: Krysl <krysl@qq.com>

---
+++ README.md
@@ -0,0 +1,4 @@
+# Memory TreeMap for [ProcessHacker](https://github.com/processhacker/processhacker)
+
+The image below use CSV data exported by ProcessHacker:
+![](./doc/img/screenshot.png)
---
+++ vite.config.ts
@@ -0,0 +1,44 @@
+import { defineConfig, mergeConfig, UserConfig, UserConfigFn } from "vite";
+
+const config = defineConfig((env) => {
+  const baseConfig = defineConfig({
+    root: "./src/",
+    base: "./",
+    build: {
+      // sourcemap: "inline",
+      // minify: false,
+      // lib: {
+      //   entry: "index.ts",
+      //   name: "ProcessHackarTreemapMemory",
+      //   // fileName: (format) => `ProcessHackarTreemapMemory.${format}.js`,
+      // },
+      outDir: "../dist"
+    },
+  });
+  let customConfig: UserConfig;
+  if (env.command === "serve") {
+    customConfig = {
+      // serve 独有配置
+      // server: {
+      //   fs:{
+      //     allow: true,
+      //   },
+      //   proxy: {
+      //     "^/_m/.*": {
+      //       target: "http://localhost:3000",
+      //       changeOrigin: true,
+      //       rewrite: (path) => path.replace(/^\/_m/, ""),
+      //     },
+      //   },
+      // },
+    };
+  } else {
+    customConfig = {
+      // build 独有配置
+
+    };
+  }
+  return mergeConfig(baseConfig, customConfig);
+}) as UserConfigFn;
+
+export default config;
