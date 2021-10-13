3f8569de503529c67fcc53115ea463d6255f1568
Krysl <krysl@qq.com>
2021-10-14 05:56:41 +0800

    add more color mode
    Signed-off-by: Krysl <krysl@qq.com>

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,7 @@
 import { parseCSV } from './parse_csv';
 import { showTreeMap, updateData } from './renderer';
 
-function createTip(txt: string) {
-  const text = document.createElement("P");
-  text.innerText = txt;
-  text.setAttribute('style', 'font-weight: bold;');
-  return text;
-}
-
-const text = createTip(
-  'The following is for demonstration, ' +
-  'please upload the "Process Hacker Processes.csv" ' +
-  'file to show your own memory usage\'s TeeeMap'
-);
+const text = document.getElementById('tips');
 
 const inputfile = document.getElementById('inputfile');
 
@@ -26,8 +15,6 @@ inputfile.addEventListener('change', function () {
 
   fr.readAsText(this.files[0]);
 })
-
-inputfile.parentNode.appendChild(text);
 
 fetch('Process Hacker Processes.csv')
   .then(function (response) {
--- src/renderer.ts
+++ src/renderer.ts
@@ -1,34 +1,118 @@
 import * as d3 from 'd3';
 import { formatBytes, uid } from './utils';
+import * as d3scalechromatic from 'd3-scale-chromatic';
 
 let d3Node: d3.Selection<d3.BaseType | SVGGElement, unknown, d3.BaseType | SVGGElement, unknown>;
 
-const height = document.body.clientHeight - 23;
-const width = document.body.clientWidth
-const color = d3.scaleSequential([8, 0], d3.interpolateMagma);
+const height = () => document.body.clientHeight - document.getElementById('titlebar').clientHeight;
+const width = () => document.body.clientWidth
 // const color = d3.scaleOrdinal(d3.schemeCategory10);
+
+const colorMap: Map<string, string> = new Map<string,string>;
+const ramdomByNameColor = (d: d3.HierarchyNode) => {
+  if (d.parent === null) {
+    d.color = "#888888";
+    return d.color
+  }
+  const name = d.data.name.replace('+...', '');
+  let c: string;
+  if (colorMap.has(name)) {
+    c = colorMap.get(name);
+  } else {
+    c = Math.random() * 360;
+    colorMap.set(name, c);
+  }
+  return d3.hsl(
+    c,
+    0.65,
+    0.5 + 0.05 * d.depth
+  ).toString()
+}
+const randomByNameStr = 'random by name';
+const colorMode: Map<string, Functon> = new Map<string, Functon>([
+  [randomByNameStr, ramdomByNameColor],
+])
+
+for (const prop in d3scalechromatic) {
+  if (prop.startsWith('interpolate')) {
+    console.log(prop)
+    const fn = d3scalechromatic[prop]
+    console.log(fn)
+    colorMode.set(prop, d => d3.scaleSequential([8, 0], fn)(d.height))
+    colorMode.set(prop + '2', d => d3.scaleSequential([8, 0], fn)(d.depth))
+  }
+}
+const colorModeSelect = document.getElementById('color-mode');
+let cfgColorMode = localStorage.getItem('color-mode');
+if (cfgColorMode === null) {
+  localStorage.setItem('color-mode', randomByNameStr);
+  cfgColorMode = randomByNameStr;
+}
+for (let [key, value] of colorMode) {
+  var opt = document.createElement('option');
+  opt.value = key;
+  opt.innerHTML = key;
+  if (key == cfgColorMode) {
+    opt.selected = true;
+  }
+  colorModeSelect.appendChild(opt);
+}
+let colorFn = colorMode.get(colorModeSelect.value);
+console.log(colorModeSelect.value)
+
+colorModeSelect.onchange = (ev) => {
+  colorFn = colorMode.get(colorModeSelect.value);
+  localStorage.setItem('color-mode', colorModeSelect.value);
+  showTreeMap(null);
+}
+
+const sortBySizeCheckBox = document.getElementById('sort-by-size');
+let cfgSortBySize = localStorage.getItem('sort-by-size');
+cfgSortBySize = cfgSortBySize == 'true' ? true : false;
+if (cfgSortBySize === null) {
+  localStorage.setItem('sort-by-size', true);
+  cfgSortBySize = true;
+}
+sortBySizeCheckBox.checked = cfgSortBySize;
+sortBySizeCheckBox.onchange = (ev) => {
+  localStorage.setItem('sort-by-size', sortBySizeCheckBox.checked);
+  cfgSortBySize = sortBySizeCheckBox.checked;
+  showTreeMap(null);
+}
+
+
+document.body.onresize = () => showTreeMap(null);
+
 const format = d3.format(",d");
 
 const treemap = data => d3.treemap()
   // .tile(d3.treemapBinary)
   // .tile(d3.treemapSquarify)
-  .size([width, height])
+  .size([width(), height()])
   .paddingOuter(3)
   .paddingTop(19)
   .paddingInner(1)
   .round(true)
   (
-    d3
-      .hierarchy(data)
-      .sum(d => d.value)
-      .sort((a, b) =>
-        // -(b.data.index - a.data.index)
-        b.value - a.value
-      )
+    cfgSortBySize ?
+      d3
+        .hierarchy(data)
+        .sum(d => d.value)
+        .sort((a, b) =>
+          // -(b.data.index - a.data.index)
+          b.value - a.value
+        ) :
+      d3
+        .hierarchy(data)
+        .sum(d => d.value)
   )
 
-
-export function showTreeMap(data: TreeNode): SVGSVGElement {
+let preData: TreeNode;
+export function showTreeMap(data: TreeNode | null): SVGSVGElement {
+  if (data === null) {
+    data = preData;
+  } else
+    preData = data
   const root = treemap(data);
 
   const shadow = uid("shadow");
@@ -43,7 +127,7 @@ export function showTreeMap(data: TreeNode): SVGSVGElement {
     .select('#d3-output')
     .append('svg')
     .attr("id", 'd3-output-svg')
-    .attr("viewBox", [0, 0, width, height])
+    .attr("viewBox", [0, 0, width(), height()])
     .style("font", "10px sans-serif");
   svg.append("filter")
     .attr("id", shadow.id)
@@ -55,7 +139,7 @@ export function showTreeMap(data: TreeNode): SVGSVGElement {
   const node = svg.selectAll("g")
     .data(d3.group(root, d => d.depth))
     .join("g")
-    // .attr("filter", shadow)
+    .attr("filter", shadow)
     .selectAll("g")
     .data(d => d[1])
     .join("g")
@@ -63,15 +147,21 @@ export function showTreeMap(data: TreeNode): SVGSVGElement {
 
   node.append("title")
     .text(d =>
-      `${d.ancestors().reverse().map(d => d.data.name).join("/")}\n` +
-      `${formatBytes(d.value)}\n` +
-      `${d.data.index}\n` +
-      `${d.data.commandLine}`
+      `${d.ancestors().reverse().map(
+        (d, index) => (index == 0) ? null : (
+          '\n' +
+          '\t'.repeat(index - 1) +
+          (index > 1 ? '↪ ' : '') +
+          d.data.name.replace('+...', '')
+        )
+      ).join("")}\n` +
+      `memory:\n\t${formatBytes(d.value)}\n` +
+      // `index:\n\t${d.data.index}\n` +
+      `cmd:\n\t${d.data.commandLine?.replaceAll(' -', '\n\t\t-')}`
     );
-
   node.append("rect")
     .attr("id", d => (d.nodeUid = uid("node")).id)
-    .attr("fill", d => color(d.height))
+    .attr("fill", d => colorFn(d))
     // .attr("fill", d => color(d.data.name))
     // .attr("fill-opacity", (d, i, nodes) => i === nodes.length - 1 ? 0.7 : null)
     // .attr("fill-opacity", (d, i, nodes) => 0.7)
@@ -86,10 +176,17 @@ export function showTreeMap(data: TreeNode): SVGSVGElement {
   node.append("text")
     .attr("clip-path", d => d.clipUid)
     .selectAll("tspan")
-    .data(d => d.data.name.split(/(?=[A-Z][^A-Z])/g)
-      .concat(formatBytes(d.value))
-      .concat(d.data.description)
-      // .concat(d.data.commandLine)
+    .data(d => {
+      let info = (d.data.name as string).split(/(?=[A-Z][^A-Z ])/g);
+      // let info = (d.data.name as string).split(/[ .]/g);
+      // if (d.height != 0)
+      //   info = ['[', ...info, ']='];
+      info = info
+        .concat(formatBytes(d.value))
+        .concat(d.data.description)
+      // .concat(d.data.commandLine)}
+      return info;
+    }
     )
     .join("tspan")
     .text(d => d);
